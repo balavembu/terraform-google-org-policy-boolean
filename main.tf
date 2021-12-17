0098b1ab29b5b68fad2c1c488c3939734a542eeb
balavembu <90377726+balavembu@users.noreply.github.com>
2021-12-17 14:14:30 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 /******************************************
-  Organization policy (boolean constraint)
+  Project policy (boolean constraint)
  *****************************************/
-resource "google_organization_policy" "org_policy_boolean" {
-  count = local.organization && local.boolean_policy ? 1 : 0
+resource "google_project_organization_policy" "project_policy_boolean" {
+  #count = local.project && local.boolean_policy ? 1 : 0
 
-  org_id     = var.organization_id
+  project    = var.project_id
   constraint = var.constraint
 
   boolean_policy {
